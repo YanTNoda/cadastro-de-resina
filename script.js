8564416c17d8a8ee05ed07a8f9dfa9319cf9928c
YanTNoda <ytn.work97@gmail.com>
2024-12-10 12:26:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function show(msg){
 console.log(msg);
 }
+const body = document.body;
 var app = document.createElement('div');
-app.setAttribute('background-color','red')
+app.setAttribute('background-color','red');
+app.appendChild(document.createTextNode('hi'));
+body.appendChild(app);
